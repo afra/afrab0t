e4d9739f1d1e4296a19b79ec23a3b88dd2d95c06
jaseg <code@jaseg.net>
2014-06-13 17:09:40 +0200

    Small cleanups, added op/kick functionality

--- afrab0t.py
+++ afrab0t.py
@@ -15,6 +15,8 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr, is_channel
 import pyimgur
 import praw
 import sqlite3
+from contextlib import contextmanager
+import settings
 
 class Afrabot(irc.bot.SingleServerIRCBot):
 	def __init__(self, db, channel, nickname, server, port=6667):
@@ -28,11 +30,38 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 		self.reddit = praw.Reddit(user_agent='AfRAb0t/0.23 by jaseg')
 		self.moincount = 0
 
+	def send(self, target, msg):
+		self.connection.privmsg(target, msg)
+
+	def sendchan(self, msg):
+		self.connection.privmsg(self.channel, msg)
+
+	def identify(self):
+		self.send('NickServ', 'identify '+settings.NICKSERV_PASSWORD)
+	
+	def regain(self):
+		self.send('NickServ', ' '.join(('regain', self.nick, settings.NICKSERV_PASSWORD)))
+	
+	def get_op(self, op=True):
+		self.send('ChanServ', ' '.join(('OP' if op else 'DEOP', self.channel, self.nick)))
+
+	@contextmanager
+	def op(self):
+		self.get_op()
+		yield
+		self.get_op(False)
+
+	def kick(self, nick):
+		with self.op():
+			self.connection.kick(self.channel, nick)
+
 	def on_nicknameinuse(self, c, e):
 		c.nick(c.get_nickname() + "_")
+		self.regain()
 
 	def on_welcome(self, c, e):
 		c.join(self.channel)
+		self.identify()
 
 	def on_privmsg(self, c, e):
 		self.do_command(e, e.arguments[0], e.source.nick, e.source.nick)
@@ -40,21 +69,29 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 	def on_pubmsg(self, c, e):
 		nick = e.source.nick
 		target = e.target if is_channel(e.target) else nick
+		def reply(msg):
+			self.send(target, msg)
+		def dm(msg):
+			self.send(nick, msg)
 		line = UnicodeDammit(e.arguments[0]).unicode_markup
 		a = line.split(":", 1)
 		if len(a) > 1 and a[0].lower() == self.nick:
-			self.do_command(e, a[1].strip().lower(), nick, target)
+			self.do_command(e, a[1].strip().lower(), nick, target, reply, dm)
 			return
 
 		# zeltofilter
 		if 'zeltoph' in nick:
 			return
+
+		foo = settings.VIPS.get(nick, 0)
+		if random() < foo:
+			self.kick(nick)
 		
 		match = re.match('^({} *:)? *chaos-?([☆★☼☀*]|sternchen) *: ?(.*)$'.format(self.nick), line)
 		if match:
 			newcs = match.group(3)
 			self.chaossternchen.append(newcs)
-			c.privmsg(self.channel, 'Chaos-☆ Nr. {} notiert: {}'.format(len(self.chaossternchen), newcs))
+			self.sendchan('Chaos-☆ Nr. {} notiert: {}'.format(len(self.chaossternchen), newcs))
 			return
 
 		if line.startswith('.wiki '):
@@ -62,35 +99,35 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 			if re.match('^[-_+\w]+$', wikipage):
 				wikiurl = 'https://afra-berlin.de/dokuwiki/doku.php?id={}'.format(wikipage)
 				if 'Dieses Thema existiert noch nicht' in requests.get(wikiurl).text:
-					c.privmsg(target, "I'm sorry, I can't find a wiki page with that name.")
+					reply("I'm sorry, I can't find a wiki page with that name.")
 				else:
-					c.privmsg(target, wikiurl)
+					reply(wikiurl)
 			else:
-				c.privmsg(target, 'Try to troll somebot else.')
+				reply('Try to troll somebot else.')
 			return
 
 		if line == 'wat?':
-			c.privmsg(target, "I don't have a clue.")
+			reply("I don't have a clue.")
 			return
 		if re.match('^hail eris[.!]* ', line.lower()):
-			c.privmsg(target, "All Hail Discordia!")
+			reply("All Hail Discordia!")
 			return
 		m = re.findall('(^|\s)?(gh?ah?nh?dh?ih?)(\s|$)?', line, re.IGNORECASE)
 		for _1,match,_2 in m:
 			if not re.match('(^|\s)?gandhi(\s|$)?', match, re.IGNORECASE):
-				c.privmsg(target, "It's spelled Gandhi")
+				reply("It's spelled Gandhi")
 				return
 		if re.search('https?://[-a-z0-9.]*facebook.com', line.lower()):
-			c.privmsg(target, 'A facebook link? srsly? Get some self-respect!')
+			reply('A facebook link? srsly? Get some self-respect!')
 			return
 		match = re.search('https?://pr0gramm.com/#(newest/\*/[0-9/]*)', line.lower())
 		if match:
-			c.privmsg(target, 'Fixed that pr0gramm link for you: http://pr0gramm.com/static/'+match.group(1))
+			reply('Fixed that pr0gramm link for you: http://pr0gramm.com/static/'+match.group(1))
 			return
 		if line == 'moin':
 			self.moincount += 1
 			if self.moincount == 5:
-				c.privmsg(target, 'moin')
+				reply('moin')
 			return
 		else:
 			self.moincount = 0
@@ -100,19 +137,19 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 				db.execute("DELETE FROM etas WHERE nick=?", (nick,))
 				if eta:
 					db.execute("INSERT INTO etas VALUES (DATETIME('now'), ?, ?)", (nick, eta))
-			c.privmsg(nick, 'ETA registered. Thanks!')
+			dm('ETA registered. Thanks!')
 			return
 		if 'union' in line.lower():
-			c.privmsg(target, 'Uniohon: https://www.youtube.com/watch?v=ym3Giin2C8k')
+			reply('Uniohon: https://www.youtube.com/watch?v=ym3Giin2C8k')
 			return
 		m = re.findall('(^|\s)(afra)(\s|$)', line, re.IGNORECASE)
 		for match in m:
 			if match[1] != 'AfRA' and random() < 0.1:
-				c.privmsg(target, "I'm sure you meant AfRA, not "+match[1])
+				reply("I'm sure you meant AfRA, not "+match[1])
 				return
 
 	def on_dccmsg(self, c, e):
-		c.privmsg("Störe meine Kreise nicht.")
+		pass
 
 	def on_dccchat(self, c, e):
 		if len(e.arguments) != 2:
@@ -126,7 +163,7 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 				return
 			self.dcc_connect(address, port)
 
-	def do_command(self, e, cmd, nick, target):
+	def do_command(self, e, cmd, nick, target, reply, dm):
 		c = self.connection
 
 		emoticontable = {
@@ -153,26 +190,26 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 				}
 		for emoticon, uchar in emoticontable.items():
 			if re.findall('(^|\W)'+re.escape(emoticon)+'(\W|$)', cmd) and random() < 0.333:
-				c.privmsg(target, 'Did you mean {} (U+{:x}) with “{}”?'.format(uchar, ord(uchar), emoticon))
+				reply('Did you mean {} (U+{:x}) with “{}”?'.format(uchar, ord(uchar), emoticon))
 				break
 
 		if cmd.startswith('open'):
 			if '?' in cmd or '‽' in cmd:
 				if cmd.count('?') >= 5:
-					c.privmsg(self.channel, 'afrabot: open?')
+					self.sendchan('afrabot: open?')
 					return
 				if self.lastopen:
 					if self.spaceopen:
-						c.privmsg(target, 'Space was last marked open on '+self.lastopen)
+						reply('Space was last marked open on '+self.lastopen)
 					else:
-						c.privmsg(target, 'Space was last marked closed on '+self.lastopen)
+						reply('Space was last marked closed on '+self.lastopen)
 				else:
-					c.privmsg(target, "I don't know when was the last time the space was open.")
+					reply("I don't know when was the last time the space was open.")
 			else:
 				if cmd.count('!') > 5:
-					c.privmsg(target, 'u mad bro?')
+					reply('u mad bro?')
 					return
-				c.privmsg(self.channel, 'Space is open!')
+				self.sendchan('Space is open!')
 				self.lastopen = time.ctime()
 				self.spaceopen = True
 			return
@@ -180,18 +217,18 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 			if '?' in cmd or '‽' in cmd:
 				if self.lastopen:
 					if self.spaceopen:
-						c.privmsg(target, 'Space was last marked open on '+self.lastopen)
+						reply('Space was last marked open on '+self.lastopen)
 					else:
-						c.privmsg(target, 'Space was last marked closed on '+self.lastopen)
+						reply('Space was last marked closed on '+self.lastopen)
 				else:
-					c.privmsg(target, "I don't know when was the last time the space was closed.")
+					reply("I don't know when was the last time the space was closed.")
 			else:
 				if cmd.count('!') > 5:
-					c.privmsg(target, 'u mad bro?')
+					reply('u mad bro?')
 					return
-				c.privmsg(self.channel, 'Space is closed! Please remember to follow the shutdown protocol.')
+				self.sendchan('Space is closed! Please remember to follow the shutdown protocol.')
 				if target != self.channel:
-					c.privmsg(target, 'Please remember to follow the shutdown protocol.')
+					reply('Please remember to follow the shutdown protocol.')
 				self.lastopen = time.ctime()
 				self.spaceopen = False
 			return
@@ -199,20 +236,20 @@ class Afrabot(irc.bot.SingleServerIRCBot):
 			cs = self.chaossternchen
 			if 'genug' in cmd:
 				self.chaossternchen = []
-				c.privmsg(target, 'Plenum beendet.')
+				reply('Plenum beendet.')
 			else:
-				c.privmsg(target, 'Aye! So far, there are {} Chaos-☆'.format(len(cs)) + ('.' if len(cs) == 0 else ':'))
+				reply('Aye! So far, there are {} Chaos-☆'.format(len(cs)) + ('.' if len(cs) == 0 else ':'))
 			for entry in enumerate(cs):
-				c.privmsg(target, 'Chaos-☆ {}: {}'.format(*entry))
+				reply('Chaos-☆ {}: {}'.format(*entry))
 			return
 		csmatch = re.match('^ *(delete|remove) +chaos-?([☆★☼☀*]|sternchen) *([0-9]+)[.!]{,5}$', cmd)
 		if csmatch:
 			try:
 				num = int(csmatch.group(3))
 				del self.chaossternchen[num]
-				c.privmsg(target, 'Chaos-☆ {} deleted.'.format(num))
+				reply('Chaos-☆ {} deleted.'.format(num))
 			except:
-				c.privmsg(target, 'wut?')
+				reply('wut?')
 			return
 		if re.match('^help[?!‽.]*$', cmd):
 			helptext = """open|closed? - query whether space is open
@@ -224,7 +261,7 @@ plenum - list plenum topics
 ... and many more, doc urgently needed. Please submit PRs on github: https://github.com/afra/afrab0t
 """
 			for line in helptext.splitlines():
-				c.privmsg(target, line)
+				reply(line)
 			return
 		if re.match('^shutdown[?‽]*$', cmd):
 			helptext = """* Fenster schließen (Beim rechten Fenster muss ein Hebel unten am Fenster betätigt werden. Bitte stellt sicher, dass beide Fenster dicht geschlossen sind.)
@@ -238,21 +275,21 @@ plenum - list plenum topics
 * Raum-, Aufgangs- und Haustür verschließen
 """
 			for line in helptext.splitlines():
-				c.privmsg(target, line)
+				reply(line)
 			return
 		if cmd == 'ponies?':
-			c.privmsg(target, 'yes please!')
+			reply('yes please!')
 			return
 		if re.match('^ *tell +afrab[o0]t +', cmd):
-			c.privmsg(target, 'what is your problem?')
+			reply('what is your problem?')
 			return
 		if cmd.rstrip('?') in ('where', 'location', 'wo'):
-			c.privmsg(target, 'AfRA e.V. is located at Herzbergstr. 55, 10365 Berlin, 2.HH/Aufgang B, 3. floor on the'
+			reply('AfRA e.V. is located at Herzbergstr. 55, 10365 Berlin, 2.HH/Aufgang B, 3. floor on the'
 					'left (Rm 3.08). Public transport: Tram M8, 21, 37 & Bus 256, N56, N50 → Herzbergstr./Siegfriedstr.'
 					'Door closed? Try +49-176-29769254 !')
 			return
 		if cmd.rstrip('?') in ('tel', 'telefon', 'telephone', 'phone', 'handy', 'fon'):
-			c.privmsg(target, "Locked out? Wanna know what's up at AfRA? Try +49-176-29769254 !")
+			reply("Locked out? Wanna know what's up at AfRA? Try +49-176-29769254 !")
 			return
 		if cmd.rstrip('?!.') in ('cats', 'katzen', 'kittens', 'kätzchen'):
 			try:
@@ -260,14 +297,14 @@ plenum - list plenum topics
 				index, item = next((i,s) for i,s in enumerate(submissions) if s.url not in self.catpiccache and not s.stickied and not s.is_self)
 				self.catpiccache.append(item.url)
 				if index != 5:
-					c.privmsg(target, 'Got some cats for you: '+item.url)
+					reply('Got some cats for you: '+item.url)
 				else:
-					c.privmsg(target, "Gee, you really like those cat things, don't you? You know, I could use some love, too: https://github.com/afra/afrab0t")
+					reply("Gee, you really like those cat things, don't you? You know, I could use some love, too: https://github.com/afra/afrab0t")
 			except StopIteration:
-				c.privmsg(target, 'The intertubes are empty.')
+				reply('The intertubes are empty.')
 			return
 		if cmd.rstrip('?!.') in ('answer', 'antworte', 'antwort'):
-			c.privmsg(target, '42')
+			reply('42')
 			return
 		# ETA handling
 		if cmd.rstrip('?') in ('etas', 'who', 'da'):
@@ -275,9 +312,9 @@ plenum - list plenum topics
 				db.execute("DELETE FROM etas WHERE timestamp < DATETIME('now', '-1 day')")
 			etas = ', '.join(nick+': '+eta for nick,eta in db.execute("SELECT nick, eta FROM etas").fetchall())
 			if etas:
-				c.privmsg(target, 'Current ETAs: '+etas)
+				reply('Current ETAs: '+etas)
 			else:
-				c.privmsg(target, 'No ETAs have been announced yet.')
+				reply('No ETAs have been announced yet.')
 			return
 		# key handling
 		keycmd = re.match('key ([\w]+) to ([\w]+)( *: *.*)?', cmd)
@@ -287,13 +324,13 @@ plenum - list plenum topics
 				keystatelist = keystate.split(', ')
 				fromnick, tonick, comment = keycmd.groups()
 				if not fromnick in keystatelist:
-					c.privmsg(target, 'According to my information, as of now {} does not have a key. Current key'
+					reply('According to my information, as of now {} does not have a key. Current key'
 							'holders are {}.'.format(fromnick, keystate))
 					return
 				keystatelist[keystatelist.index(fromnick)] = tonick
 				keystate = ', '.join(keystatelist)
 				db.execute("INSERT INTO keylog VALUES (DATETIME('now'),?,?,?,?)", (fromnick, tonick, keystate, comment))
-				c.privmsg(self.channel, 'Key transfer: {}→{}. Current key holders: {}'.format(fromnick, tonick, keystate))
+				self.sendchan('Key transfer: {}→{}. Current key holders: {}'.format(fromnick, tonick, keystate))
 			return
 		if cmd.rstrip('?') == 'progress':
 			t = datetime.datetime.now().time()
@@ -303,28 +340,28 @@ plenum - list plenum topics
 			foo = round(67*p)
 			bar = '='*foo
 			space = ' '*(67-foo)
-			c.privmsg(target, '['+bar+'>'+space+'] ({:.2f}%)'.format(p*100))
+			reply('['+bar+'>'+space+'] ({:.2f}%)'.format(p*100))
 			return
 		if cmd.startswith('keystate '):
 			keystate = re.split('[,;/: ]*', cmd)[1:]
 			self.db.execute("INSERT INTO keylog VALUES (DATETIME('now'),'','',?,'')", (', '.join(keystate),))
-			c.privmsg(self.channel, 'Key status set. Current key holders: {}'.format(', '.join(keystate)))
+			self.sendchan('Key status set. Current key holders: {}'.format(', '.join(keystate)))
 			return
 		keylog = re.match('keylog *([0-9]*)', cmd)
 		if keylog:
 			num = max(50, int(keylog.group(1) or 8))
-			c.privmsg(nick, 'The latest {} key log entries:'.format(num))
+			dm('The latest {} key log entries:'.format(num))
 			loglines = self.db.execute("SELECT * FROM keylog ORDER BY timestamp DESC LIMIT ?", (num,))
 			for timestamp, fromnick, tonick, keystate, comment in reversed(loglines):
-				c.privmsg(nick, '{}: {}→{}; Key holders {}; Comment: "{}"'.format(
+				dm('{}: {}→{}; Key holders {}; Comment: "{}"'.format(
 						timestamp, fromnick, tonick, keystate, comment))
-			c.privmsg(nick, 'EOL')
+			dm('EOL')
 			return
 		if cmd.startswith("fuck you"):
-			c.privmsg(target, 'Fucking is entirely unnecessary: I can reproduce via copy-and-paste!')
+			reply('Fucking is entirely unnecessary: I can reproduce via copy-and-paste!')
 			return
 		if cmd.startswith("geh kacken"):
-			c.privmsg(target, 'Command "kacken" not implemented. You are welcome to submit a pull request on github at https://github.com/afra/afrab0t')
+			reply('Command "kacken" not implemented. You are welcome to submit a pull request on github at https://github.com/afra/afrab0t')
 			return
 		# fall-through
 		c.notice(nick, 'I don\'t know what you mean with "{}"'.format(cmd))
